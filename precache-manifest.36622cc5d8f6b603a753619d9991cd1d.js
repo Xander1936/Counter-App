b7ca6964461f5601379611b3c1a8037396b92ed7
ALex_Xander <alexandrehervemassodamambeleck@gmail.com>
2022-10-29 19:59:09 +0100

    Updates

---
+++ precache-manifest.36622cc5d8f6b603a753619d9991cd1d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "efb6508b3ab21b0132f8",
+    "url": "/Counter-App/static/css/main.f9375b70.chunk.css"
+  },
+  {
+    "revision": "efb6508b3ab21b0132f8",
+    "url": "/Counter-App/static/js/main.efb6508b.chunk.js"
+  },
+  {
+    "revision": "67911696f9bf7b2e8777",
+    "url": "/Counter-App/static/js/1.67911696.chunk.js"
+  },
+  {
+    "revision": "f153700f1ce14bf8bd64",
+    "url": "/Counter-App/static/js/runtime~main.f153700f.js"
+  },
+  {
+    "revision": "e2481c57275ea05296cf002b1e517e4a",
+    "url": "/Counter-App/index.html"
+  }
+];
